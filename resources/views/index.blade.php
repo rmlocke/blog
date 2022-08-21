31b765647533eed9a9123d75dd1c06390593bb52
rmlocke <rmlocke@users.noreply.github.com>
2022-08-21 14:13:59 +0100

    Show message when there are no posts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,24 +4,30 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @foreach ( $posts as $post )
-                <div class="post preview">
-                    <a href="/posts/{{ $post->id }}">
-                        <h2 class="title">{{ $post->title }}</h2>
-                    </a>
-                    <div class="snippet">
-                        {!! $post->content !!}
-                    </div>
-                    <p class="post-meta">
-                        @if(isset($post->user))
-                        Posted by <a href="{{ route('users.show', $post->user)}}">{{ $post->user->name }}</a>
-                        @endif
-                        <span class="post-date">at {{ $post->created_at }}</span>
-                    </p>
-                </div>
-            @endforeach
 
-            {!! $posts->links() !!}
+            @if (count($posts))
+                @foreach ( $posts as $post )
+                    <div class="post preview">
+                        <a href="/posts/{{ $post->id }}">
+                            <h2 class="title">{{ $post->title }}</h2>
+                        </a>
+                        <div class="snippet">
+                            {!! $post->content !!}
+                        </div>
+                        <p class="post-meta">
+                            @if(isset($post->user))
+                            Posted by <a href="{{ route('users.show', $post->user)}}">{{ $post->user->name }}</a>
+                            @endif
+                            <span class="post-date">at {{ $post->created_at }}</span>
+                        </p>
+                    </div>
+                @endforeach
+
+                {!! $posts->links() !!}
+            @else
+                <p>We currently have no posts.</p>
+            @endif
+
         </div>
     </div>
 </div>
